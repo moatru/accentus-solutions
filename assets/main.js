7e3852be89eea5bad6481bfd28a8a36b26517082
moatru <moasteve@gmail.com>
2020-09-16 12:34:48 -0400

    responsive homepage / nav start

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,59 @@
+$(document).ready(function() {
+	//make sure the page is ready befor running the scripts
+
+  // hamburger 2
+  $( document ).ready(function() {
+     $(".burger-button").click(function(){
+       $(".burger-button").toggleClass("active");
+       $(".burger-menu").slideToggle();
+     });
+
+    $(".burger-menu").click(function(){
+      $(".burger-button").toggleClass("active");
+      $(".burger-menu").slideToggle();
+    });
+  });
+  // end
+
+	// This is the pop-up gallery
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title');
+			}
+		}
+	});
+  // end
+
+// FAQ Accordian
+	var allPanels = $('.accordion > dd').hide();
+
+		$('.accordion > dt > a').click(function() {
+
+			allPanels.slideUp();
+
+	    $(this).filter('.open').removeClass('open').addClass('closing')
+	    $('.accordion > dt > a').removeClass('open');
+	  $(this).not(".closing").addClass('open').parent().next().slideDown();
+	    $('.accordion > dt > a').removeClass('closing');
+
+	    return false;
+		});
+
+
+});
+// end accordian
+
 // SLIDER
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -27,54 +83,4 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
-
-
-
-
-$(document).ready(function() {
-	//make sure the page is ready befor running the scripts
-	// This is the hamburger menu
-	$(".hamburger").click(function(){
-
-	  $(this).toggleClass('active');
-	  $(".mobile-menu").fadeToggle();
-
-	});
-	// end hamburger menu
-
-	// This is the pop-up gallery
-	$('.popup-gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-			titleSrc: function(item) {
-				return item.el.attr('title');
-			}
-		}
-	});
-
-	var allPanels = $('.accordion > dd').hide();
-
-		$('.accordion > dt > a').click(function() {
-
-			allPanels.slideUp();
-
-	    $(this).filter('.open').removeClass('open').addClass('closing')
-	    $('.accordion > dt > a').removeClass('open');
-	  $(this).not(".closing").addClass('open').parent().next().slideDown();
-	    $('.accordion > dt > a').removeClass('closing');
-
-	    return false;
-		});
-
-
-});
-// end accordian
+//END
